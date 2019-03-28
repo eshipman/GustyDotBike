4d6a0e777ab58d0d4303696b2e212b0d7ac43414
Evan Shipman <eshipman@umw.edu>
2019-03-28 13:14:47 -0400

    Reordered variable declaration and added added 'assume yes' to package install

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-JAVA_PATH="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
-
 # Install the dependencies
-sudo apt-get install apache2 openjdk-8-jre
+sudo apt-get -y install apache2 openjdk-8-jre
+
+JAVA_PATH="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
 
 # Download the Mooshak installer
 wget http://mooshak2.dcc.fc.up.pt/install/MooshakInstaller.jar
