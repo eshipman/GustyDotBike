cd2d01a4940f560876f1b4305ec47dd25f3cf9d3
Evan Shipman <eshipman@umw.edu>
2019-03-28 13:49:34 -0400

    Updated the package list and started wordpress install commands

--- installer.sh
+++ installer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+WORDPRESS_CONFIG="./wordpress.tar.gz"
+
 # Install the dependencies
-sudo apt-get -y install apache2 openjdk-8-jre
+sudo apt-get update
+sudo apt-get -y install apache2 mysql-server php libapache2-mod-php php-mysql \
+    php-curl php-gd php-mbstring php-xml php-xmlrpc openjdk-8-jre
 
 # Verify apache is running
 sudo systemctl start apache2
@@ -24,3 +28,12 @@ sudo "${JAVA_PATH}" -jar MooshakInstaller.jar -cui <<EOF
 
 
 EOF
+
+# Install wordpress
+
+# the user must be a sudoer
+# sudo chown -R user:www-data /var/www/html
+sudo find /var/www/html -type d -exec chmod g+s {} \;
+sudo chmod g+w /var/www/html/wp-content
+sudo chmod -R g+w /var/www/html/wp-content/themes
+sudo chmod -R g+w /var/www/html/wp-content/plugins
