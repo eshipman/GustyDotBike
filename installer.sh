c0139f2d09e1cff445f8f196e9b0fa90e6ed0fb8
Evan Shipman <eshipman@umw.edu>
2019-03-28 13:34:50 -0400

    Fixed mooshak install inputs and added apache start

--- installer.sh
+++ installer.sh
@@ -3,15 +3,20 @@
 # Install the dependencies
 sudo apt-get -y install apache2 openjdk-8-jre
 
+# Verify apache is running
+sudo systemctl start apache2
+
 JAVA_PATH="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
 
 # Download the Mooshak installer
 wget http://mooshak2.dcc.fc.up.pt/install/MooshakInstaller.jar
 
 # Run the installer with the correct version of Java
-"${JAVA_PATH}" -jar MooshakInstaller.jar <<EOF
-0
-yes
+sudo "${JAVA_PATH}" -jar MooshakInstaller.jar -cui <<EOF
+
+
+
+
 
 
 
